720108478f1f635fc30f728d5651dfc8c25dbbd8
Harsh Gupta <150253719+harshgupta2125@users.noreply.github.com>
2025-08-04 23:05:37 +0530

    19878

--- products/serializers.py
+++ products/serializers.py
@@ -5,3 +5,9 @@ class ItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = Item
         fields = '__all__'
+
+class MenuItemSerializer(serializers.Serializer):
+    name= serializers.CharField(max_length=255)
+    description= serializers.CharField()
+    price= serializers.DecimalField(max_length=5, decimal_places=2)
+    
