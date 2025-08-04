cea545cba2fec39b033d2cf66ffa2e5783c53816
Harsh Gupta <150253719+harshgupta2125@users.noreply.github.com>
2025-08-04 22:56:06 +0530

    19877

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
-
+from .models import MenuItem, Order, OrderItems
 # Register your models here.
+
+
+@admin.register(MenuItem)
+class MenuItemAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'description')
+    search_fields= ('name',)
+
+class OrderItemsInline(admin.TabularInline):
+    model = OrderItem
+    fields=('menu_item', 'quantity',)
+    extra=1
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display= ('id', 'customer', 'status', 'total_amount', 'created_at')
+    readonly_fields= ('total_amount', 'created_at')
+    inline= [OrderItemsInline]
