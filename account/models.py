a1367f21cc0c9a14dc45759d4bdc7e8c38e7e56a
Harsh Gupta <150253719+harshgupta2125@users.noreply.github.com>
2025-08-04 22:31:15 +0530

    19876

--- account/models.py
+++ account/models.py
@@ -21,3 +21,30 @@ class MenuItem(models.Model):
     def __str__(self):
         return self.name
         
+class Order(models.Model):
+    customer= models.ForeignKey(User, on_delete=models.CASCADE, related_name='orders')
+    total_amount= models.DecimalField(max_digits=8, decimal_places
+    =2, default=0.00)
+    items= models.ManyToManyField(MenuItem, through='OrderItem')
+    
+    ORDER_STATUS_CHOICES=[
+        ('PENDING', 'Pending'),
+        ('PREPARING', 'Preparing'),
+        ('OUT_FOR_DELIVERY', 'Out For Delivery'),
+        ('Delivered', 'Delivered'),
+        ('CANCELLED', 'Cancelled'),
+    ]
+
+    status= models.CharField(max_length=20, choices=ORDER_STATUS_CHOICES, default='PENDING')
+    created_at= models.DateTime(auto_now_add= True)
+    def __str__(self):
+        return f"Order #{self.id} by {self.customer.username}"
+
+class OrderItem(models.Model):
+    order= models.ForeignKey(Order, on_delete=models.CASCADE)
+    menu_item= models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    quantity= models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.quantity} x {self.menu_item.name} for order #{self.order.id}"
+        
