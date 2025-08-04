c6d3ec230db0330634636d58ca69aecf57ad6f55
Harsh Gupta <150253719+harshgupta2125@users.noreply.github.com>
2025-08-04 23:10:04 +0530

    19882

--- home/views.py
+++ home/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from .models import MenuItem
 # Create your views here.
+
+def menu_page_view(request):
+    menu_items= MenuItem.objects.all()
+    context= {
+        'menu_items': menu_items
+    }
+
+    return render(request, 'menu_page.html', context)
+    
