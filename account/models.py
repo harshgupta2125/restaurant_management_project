62673c9aa302955dfbbbdc3037f369c6b2480977
Harsh Gupta <150253719+harshgupta2125@users.noreply.github.com>
2025-08-04 22:17:17 +0530

    19881

--- account/models.py
+++ account/models.py
@@ -12,3 +12,12 @@ class UserProfile(models.Model):
     def __str__(self):
         return self.user.username
         
+
+class MenuItem(models.Model):
+    name= models.CharField(max_length=255)
+    description= models.TextField()
+    price= models.DecimalFirld(max_digits=5, decimal_places=2)
+    
+    def __str__(self):
+        return self.name
+        
