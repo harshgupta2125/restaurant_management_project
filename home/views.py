448c57b7d5c0f210761439a6127edb41025074e7
Harsh Gupta <150253719+harshgupta2125@users.noreply.github.com>
2025-08-07 21:40:58 +0530

    22681

--- home/views.py
+++ home/views.py
@@ -10,3 +10,24 @@ def menu_page_view(request):
 
     return render(request, 'menu_page.html', context)
     
+
+
+def homepage_view(request):
+    restaurant_name= None
+    try:
+        restaurant_name_obj= RestaurantName.objects.first()
+        if restaurant_name_obj:
+            restaurant_name= restaurant_name_obj.name 
+
+    except RestaurantName.DoesNotExist:
+        pass
+
+    if not restaurant_name:
+        restaurant_name= getattr(setting, 'RESTAURANT_NAME', 'Default Diner')
+
+    context={
+        'restaurant_name': restaurant_name,
+    }
+
+    return render (request, 'templates/home.html', context)
+    
