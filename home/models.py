1851bbf889b72f60e7914af2712a37a29cee478a
Harsh Gupta <150253719+harshgupta2125@users.noreply.github.com>
2025-08-07 21:42:24 +0530

    22681

--- home/models.py
+++ home/models.py
@@ -5,3 +5,10 @@ class RestaurantName(models.Model):
     name= models.CharField(max_length=100, unique=True, default="My Awesome Restaurant")
     def __str__(self):
         return self.name
+
+
+class RestaurantName(models.Model):
+    name= models.CharField(max_length=100, unique=True, default="My Awesome Restaurant")
+
+    def __str__(self):
+        return self.name
