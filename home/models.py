6f03400f02f659ac0ea2275035e13aef63047c2c
Harsh Gupta <150253719+harshgupta2125@users.noreply.github.com>
2025-08-07 20:56:02 +0530

    22681

--- home/models.py
+++ home/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class RestaurantName(models.Model):
+    name= models.CharField(max_length=100, unique=True, default="My Awesome Restaurant")
+    def __str__(self):
+        return self.name
